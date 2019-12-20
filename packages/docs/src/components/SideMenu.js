2f043315d82192de541560678489273fb1a45500
Dean Harris <dean@harris.tc>
2019-12-20 19:22:51 +1000

    feat(menu): rewrite menu and use compiled components

--- packages/docs/src/components/SideMenu.js
+++ packages/docs/src/components/SideMenu.js
@@ -1,54 +1,102 @@
 import React from 'react';
 import { StaticQuery, Link, graphql } from 'gatsby';
 
+const query = graphql`
+  query{
+    allSitePage {
+      nodes {
+        path
+      }
+    }
+    site {
+      siteMetadata {
+        menu {
+          label
+          route
+          children {
+            label
+            route
+          }
+        }
+      }
+    }
+  }
+`
+
+const Menu = ({allSitePage: {nodes}, site: {siteMetadata: {menu}}}) => {
+  const activeItems = nodes.map(node => node.path);
+
+  return (
+    <nav>
+      {
+        menu.map(
+            ({label, children}) => (
+            <section key={label}>
+              <h5>{label}</h5>
+              <ul>
+                {
+                  children.map(
+                    ({label, route}) => (
+                      activeItems.includes(route) ? (
+                      <li key={route}><Link to={route}>{label}</Link></li>
+                      ):(
+                        <li key={route}>{label}</li>
+                      )
+                    )
+                  )
+                }
+              </ul>
+            </section>
+          )
+        )
+      }
+    </nav>
+  )
+
+}
+
+// data => {
+//   const groupedItems = data.site.siteMetadata.menuItems.reduce((acc, curr) => {
+//     if (!acc[curr.menu]) {
+//       acc[curr.menu] = [curr]
+//       return acc;
+//     }
+//     acc[curr.menu] = [...acc[curr.menu], curr]
+//     return acc
+//   }, {})
+
+//   const groups = Object.keys(groupedItems).map(
+//     group => groupedItems['group']
+//   );
+
+//   console.log(groupedItems, groups)
+
+
+//   const activeRoutes = data.allSitePage.nodes.reduce((acc,curr) => [...acc, curr.path], [])
+
+
+
+//   return (<nav>
+//     <section>
+//       <ul>
+//         {
+//           data.site.siteMetadata.menuItems.map((item) => (
+//             activeRoutes.includes(item.route) ? (
+//               <Link to={item.route} key={item.route}><li>{item.label}</li></Link>
+//             ):(
+//               <li key={item.route}>{item.label}</li>
+//             )
+//           )
+//           )
+//         }
+//       </ul>
+//     </section>
+//   </nav>)
+// }
+
 export const SideMenu = () => (
   <StaticQuery
-    query={
-      graphql`
-        query MenuItems{
-            allSitePage {
-              nodes {
-                path
-              }
-            }
-            site {
-              siteMetadata {
-                menuItems {
-                  label
-                  menu
-                  route
-                }
-              }
-            }
-        }`
-    }
-    render={data => {
-      const groupedItems = data.site.siteMetadata.menuItems.reduce((acc, curr) => {
-        if (!acc[curr.menu]) {
-          acc[curr.menu] = [curr]
-          return acc;
-        }
-        acc[curr.menu] = [...acc[curr.menu], curr]
-        return acc
-      }, {})
-      const activeRoutes = data.allSitePage.nodes.reduce((acc,curr) => [...acc, curr.path], [])
-      console.log(activeRoutes)
-      return (<nav>
-        <section>
-          <ul>
-            {
-              data.site.siteMetadata.menuItems.map((item) => (
-                activeRoutes.includes(item.route) ? (
-                  <Link to={item.route} key={item.route}><li>{item.label}</li></Link>
-                ):(
-                  <li key={item.route}>{item.label}</li>
-                )
-              )
-              )
-            }
-          </ul>
-        </section>
-      </nav>)
-    }}
+    query={query}
+    render={Menu}
   />
 )
