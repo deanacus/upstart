9649845fb17da242e08d2763cc432d3bddd59c3e
Dean Harris <dean@harris.tc>
2020-01-13 06:11:43 +1000

    Feature/tokens package (#11)
    * feat: start setting up some color functions
    
    * feat: update tokerns and status content

---
+++ packages/tokens/src/colors.js
@@ -0,0 +1,90 @@
+const grey = [
+  '#F1F3F5',
+  '#D5DCE1',
+  '#B7C2CC',
+  '#93A1B0',
+  '#748494',
+  '#556575',
+  '#314351',
+  '#253642',
+  '#1d2b36',
+];
+
+const primary = [
+  "#fff4e6",
+  "#ffe8cc",
+  "#ffd8a8",
+  "#ffc078",
+  "#ffa94d",
+  "#ff922b",
+  "#fd7e14",
+  "#f76707",
+  "#e8590c",
+  "#d9480f",
+];
+
+const secondary = [
+  "#e3fafc",
+  "#c5f6fa",
+  "#99e9f2",
+  "#66d9e8",
+  "#3bc9db",
+  "#22b8cf",
+  "#15aabf",
+  "#1098ad",
+  "#0c8599",
+  "#0b7285",
+];
+
+const success = [
+  "#ebfbee",
+  "#d3f9d8",
+  "#b2f2bb",
+  "#8ce99a",
+  "#69db7c",
+  "#51cf66",
+  "#40c057",
+  "#37b24d",
+  "#2f9e44",
+  "#2b8a3e",
+];
+
+const warning = [
+  "#fff9db",
+  "#fff3bf",
+  "#ffec99",
+  "#ffe066",
+  "#ffd43b",
+  "#fcc419",
+  "#fab005",
+  "#f59f00",
+  "#f08c00",
+  "#e67700",
+];
+
+const error = [
+  "#fff5f5",
+  "#ffe3e3",
+  "#ffc9c9",
+  "#ffa8a8",
+  "#ff8787",
+  "#ff6b6b",
+  "#fa5252",
+  "#f03e3e",
+  "#e03131",
+  "#c92a2a",
+];
+
+const foreground = grey[6];
+const background = grey[0];
+
+export const colors = {
+  primary,
+  secondary,
+  success,
+  error,
+  warning,
+  grey,
+  foreground,
+  background,
+};
