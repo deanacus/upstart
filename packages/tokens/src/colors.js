7d566c8b541d1131ccb83b097d233f00f2d4289c
Dean Harris <dean@harris.tc>
2020-01-13 06:10:03 +1000

    feat: update tokerns and status content

--- packages/tokens/src/colors.js
+++ packages/tokens/src/colors.js
@@ -1,84 +1,78 @@
 const grey = [
-  '#F7FAFC',
-  '#EDF2F7',
-  '#E2E8F0',
-  '#CBD5E0',
-  '#A0AEC0',
-  '#718096',
-  '#4A5568',
-  '#2D3748',
-  '#1A202C',
-  '#171923', // rgb(23, 25, 35);
-];
-
-const error = [
-  '#ffa8a8',
-  '#ff8787',
-  '#ff6b6b',
-  '#fa5252',
-  '#f03e3e',
-  '#e03131',
-  '#c92a2a', // rgb(201, 42, 42)
-];
-
-const success = [
-  '#8ce99a',
-  '#69db7c',
-  '#51cf66',
-  '#40c057',
-  '#37b24d',
-  '#2f9e44',
-  '#2b8a3e', // rgb(43, 138, 62)
-];
-
-const primary = [
-  '#ffc078',
-  '#ffa94d',
-  '#ff922b',
-  '#fd7e14',
-  '#f76707',
-  '#e8590c',
-  '#d9480f', // rgb(217, 72, 15);
-];
-
-const secondary = [
-  '#66d9e8',
-  '#3bc9db',
-  '#22b8cf',
-  '#15aabf',
-  '#1098ad',
-  '#0c8599',
-  '#0b7285', // rgb(11, 114, 133);
-];
-
-const warning = [
-  '#ffe066',
-  '#ffd43b',
-  '#fcc419',
-  '#fab005',
-  '#f59f00',
-  '#f08c00',
-  '#e67700', // rgb(230, 119, 0)
-];
-
-const charcoal = [
+  '#F1F3F5',
+  '#D5DCE1',
+  '#B7C2CC',
   '#93A1B0',
   '#748494',
   '#556575',
-  '#405261',
   '#314351',
   '#253642',
-  '#1d2b36', // rgb(29, 43, 54);
+  '#1d2b36',
 ];
 
-const ash = [
-  '#F9FAFA',
-  '#F1F3F5',
-  '#E5E9EC',
-  '#D5DCE1',
-  '#C5CED6',
-  '#B7C2CC',
-  '#A5B2BD', // rgb(165, 178, 189);
+const primary = [
+  "#fff4e6",
+  "#ffe8cc",
+  "#ffd8a8",
+  "#ffc078",
+  "#ffa94d",
+  "#ff922b",
+  "#fd7e14",
+  "#f76707",
+  "#e8590c",
+  "#d9480f",
+];
+
+const secondary = [
+  "#e3fafc",
+  "#c5f6fa",
+  "#99e9f2",
+  "#66d9e8",
+  "#3bc9db",
+  "#22b8cf",
+  "#15aabf",
+  "#1098ad",
+  "#0c8599",
+  "#0b7285",
+];
+
+const success = [
+  "#ebfbee",
+  "#d3f9d8",
+  "#b2f2bb",
+  "#8ce99a",
+  "#69db7c",
+  "#51cf66",
+  "#40c057",
+  "#37b24d",
+  "#2f9e44",
+  "#2b8a3e",
+];
+
+const warning = [
+  "#fff9db",
+  "#fff3bf",
+  "#ffec99",
+  "#ffe066",
+  "#ffd43b",
+  "#fcc419",
+  "#fab005",
+  "#f59f00",
+  "#f08c00",
+  "#e67700",
+];
+
+const error = [
+  "#fff5f5",
+  "#ffe3e3",
+  "#ffc9c9",
+  "#ffa8a8",
+  "#ff8787",
+  "#ff6b6b",
+  "#fa5252",
+  "#f03e3e",
+  "#e03131",
+  "#c92a2a",
 ];
 
 const foreground = grey[6];
@@ -91,8 +85,6 @@ export const colors = {
   error,
   warning,
   grey,
-  charcoal,
-  ash,
   foreground,
   background,
 };
