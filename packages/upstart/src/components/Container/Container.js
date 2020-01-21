47a4067ae627ddf979c560b910f2e795b4d8bf8a
Dean Harris <dean.harris@comparethemarket.com.au>
2020-01-22 07:18:58 +1000

    test(container): add tests for Container

--- packages/upstart/src/components/Container/Container.js
+++ packages/upstart/src/components/Container/Container.js
@@ -3,13 +3,18 @@ import PropTypes from 'prop-types';
 
 import { StyledContainer } from './styled';
 
-export const Container = ({ fixed, size, children }) => (
-  <StyledContainer fixed={fixed} size={size}>
+export const Container = ({
+  fixed, size, children, className,
+}) => (
+  <StyledContainer className={className} fixed={fixed} size={size}>
     {children}
   </StyledContainer>
 );
 
 Container.propTypes = {
+  /** A custom className you would like to pass to the Column */
+  className: PropTypes.string,
+
   /** Whether or not the grid container has a fixed max-width or not */
   fixed: PropTypes.bool,
 
@@ -32,6 +37,7 @@ Container.propTypes = {
 };
 
 Container.defaultProps = {
+  className: null,
   fixed: false,
   size: null,
 };
