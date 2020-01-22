fed2c4f945665cfc11a76952d3cf45871d6612fb
Dean Harris <dean.harris@comparethemarket.com.au>
2020-01-22 14:55:14 +1000

    fix: remove weird border from top nav

--- packages/docs/src/components/TopNav.js
+++ packages/docs/src/components/TopNav.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Row, Button, Flex, Column } from '@deanacus/upstart';
 
-const HeaderRow = styled(Row)`
-  border: 3px solid ${({theme}) => theme.colors.grey[500]};
-`;
+import {
+  Row, Button, Column,
+} from '@deanacus/upstart';
 
 export const TopNav = () => (
-  <HeaderRow justify="between" align="center" padding="py-3">
+  <Row justify="between" align="center" padding="py-3">
     <Column padding="p-4">
       <div><span role="img" aria-label="Pointing up emoji">☝️</span>Upstart</div>
     </Column>
     <Column padding="p-4">
       <Button href="https://github.com/deanacus/upstart" size="sm" isOutline>Github</Button>
     </Column>
-  </HeaderRow>
-)
+  </Row>
+);
+
+export default TopNav;
