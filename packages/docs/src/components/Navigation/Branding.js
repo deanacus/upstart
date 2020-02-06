5e53bfd4bacba753e7de5492459cca7c7dab59fa
Dean Harris <dean@harris.tc>
2020-02-06 17:31:42 +1000

    feat: simplify branding

--- packages/docs/src/components/Navigation/Branding.js
+++ packages/docs/src/components/Navigation/Branding.js
@@ -1,45 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import {
-  Button, Column, Link, getPadding,
-} from '@deanacus/upstart';
-
-const LinksBox = styled.div`
-  ${({ theme }) => getPadding('py-3', theme)}
-`;
+import { Column, Link } from '@deanacus/upstart';
 
 const Logo = () => (
   <Link href="/"><span role="img" aria-label="Pointing up emoji">☝️</span>Upstart</Link>
 );
 
-export const Branding = ({ links }) => (
-  <header>
-    <Column align="center">
-      <Logo />
-      <LinksBox>
-        {links.map((link) => <Button isOutline size="sm" key={link.url} href={link.url}>{link.label}</Button>)}
-      </LinksBox>
-    </Column>
-  </header>
+export const Branding = () => (
+  <Column align="center" padding="py-5">
+    <Logo />
+  </Column>
 );
-
-Branding.propTypes = {
-  links: PropTypes.arrayOf(
-    PropTypes.shape({
-      url: PropTypes.string,
-      label: PropTypes.string,
-    }),
-  ),
-};
-
-Branding.defaultProps = {
-  links: [
-    {
-      label: 'Github',
-      url: 'https://github.com/deanacus/upstart',
-    },
-  ],
-};
 
 export default Branding;
