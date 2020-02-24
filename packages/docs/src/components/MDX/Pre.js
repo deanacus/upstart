7d17a65b9f756baa7ddc3ec21b1b4d17141b793b
Dean Harris <dean@harris.tc>
2020-02-25 06:33:21 +1000

    fix: add proptypes to pre component

--- packages/docs/src/components/MDX/Pre.js
+++ packages/docs/src/components/MDX/Pre.js
@@ -1,6 +1,13 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Pre = ({ children }) => <div>{children}</div>;
+
+Pre.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(PropTypes.node),
+  ]).isRequired,
+};
 
 export default Pre;
