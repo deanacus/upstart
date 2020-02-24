367f179408aa2da023ad12ea3cda55d22129501e
Dean Harris <dean@harris.tc>
2020-02-25 07:17:29 +1000

    fix: remove search from branding as it's non-functional

--- packages/docs/src/components/Navigation/Branding.js
+++ packages/docs/src/components/Navigation/Branding.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Flex, Link, Input } from '@deanacus/upstart';
+import { Flex, Link } from '@deanacus/upstart';
 
 export const Branding = () => (
   <Flex justify="center" direction="column">
     <Link href="/">
       <span role="img" aria-label="Pointing up emoji">☝️</span>Upstart
     </Link>
-    <Input type="search" label="Search" id="search" />
   </Flex>
 );
 
