e166bc95889a402f6fbc3ee88aeb45ed21e3d1af
Dean Harris <dean@harris.tc>
2020-02-25 11:25:32 +1000

    feat: finish initial Audio component

--- packages/docs/src/components/Documentation/Status.js
+++ packages/docs/src/components/Documentation/Status.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { getRem } from '@deanacus/upstart';
+import { getRem, getMargin, getPadding } from '@deanacus/upstart';
 
 const Circle = styled.span`
   background: ${({ status, theme }) => theme.colors[status][5]};
@@ -10,6 +10,19 @@ const Circle = styled.span`
   display: inline-block;
   height: 1rem;
   width: 1rem;
+  ${({ theme, margin }) => getMargin(margin, theme)}
+`;
+
+const StyledStatus = styled.div`
+  border: .1rem solid ${({ variant, theme }) => theme.colors[variant][8]};
+  border-radius: ${({ theme }) => getRem(theme.radii.circle, theme.rootVal)};
+  color: ${({ variant, theme }) => theme.colors[variant][8]};
+  display: inline-block;
+  ${({ theme }) => getPadding('px-xs', theme)}
+`;
+
+const Content = styled.span`
+  font-size: ${({ theme }) => getRem(theme.fontSizes.xs)};
 `;
 
 const statusMap = {
@@ -20,8 +33,19 @@ const statusMap = {
   review: 'warning',
 };
 
+const statusStringMap = {
+  complete: 'Complete',
+  progress: 'In Progress',
+  planned: 'Planned',
+  retired: 'Retired',
+  review: 'In Review',
+};
+
 export const Status = ({ status }) => (
-  <Circle status={statusMap[status]} />
+  <StyledStatus variant={statusMap[status]}>
+    <Circle status={statusMap[status]} margin="mr-1" />
+    <Content>{statusStringMap[status]}</Content>
+  </StyledStatus>
 );
 
 Status.propTypes = {
--- packages/upstart/src/components/Audio/Audio.js
+++ packages/upstart/src/components/Audio/Audio.js
@@ -2,32 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Audio = ({
-  shouldAutoplay, hideControls, shouldLoop, className, children,
-}) => (
-  <audio
-    autoPlay={shouldAutoplay}
-    className={className}
-    controls={!hideControls}
-    loop={shouldLoop}
-  >
-    {children}
-    <p>Sorry, your browser doesn&apos;t support embedded audio</p>
-  </audio>
-);
+  shouldAutoplay, hideControls, shouldLoop, className, src, children,
+}) => {
+  const useSrc = src && !children;
+  return (
+    <audio
+      autoPlay={shouldAutoplay}
+      className={className}
+      controls={!hideControls}
+      loop={shouldLoop}
+      src={useSrc && src}
+    >
+      {children}
+      <p>Sorry, your browser doesn&apos;t support the audio element</p>
+    </audio>
+  );
+};
 
 Audio.propTypes = {
-
-  /** Whether or not the audio should play automatically */
-  shouldAutoplay: PropTypes.bool,
-
-  /** A custom className you would like to pass to the Component */
-  className: PropTypes.string,
-
-  /** Whether or not the controls should be hidden */
-  hideControls: PropTypes.bool,
-
-  /** Whether or not the audio file should be looped */
-  shouldLoop: PropTypes.bool,
 
   /**
    * The content of the Audio component. Should be a <source> element or React
@@ -39,13 +31,29 @@ Audio.propTypes = {
       PropTypes.node,
     ),
   ]).isRequired,
+
+  /** A custom className you would like to pass to the Component */
+  className: PropTypes.string,
+
+  /** Whether or not the controls should be hidden */
+  hideControls: PropTypes.bool,
+
+  /** Whether or not the audio should play automatically */
+  shouldAutoplay: PropTypes.bool,
+
+  /** Whether or not the audio file should be looped */
+  shouldLoop: PropTypes.bool,
+
+  /** A path to a single source. Is ignored if children are passed in, as well. */
+  src: PropTypes.string,
 };
 
 Audio.defaultProps = {
-  shouldAutoplay: false,
   className: null,
   hideControls: false,
+  shouldAutoplay: false,
   shouldLoop: false,
+  src: null,
 };
 
 export default Audio;
