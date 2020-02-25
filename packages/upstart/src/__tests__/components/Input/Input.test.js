233b07b5452ee6a8db5c817579daeede8d2d619d
Dean Harris <dean@harris.tc>
2020-02-25 14:48:39 +1000

    feat: change API for input

--- packages/upstart/src/__tests__/components/Input/Input.test.js
+++ packages/upstart/src/__tests__/components/Input/Input.test.js
@@ -5,7 +5,7 @@ import { Input } from '../../../components';
 describe('<Input />', () => {
   it('should render', () => {
     const { container } = render(
-      <Input className="test-input" id="test-input" label="Test input">
+      <Input className="test-input" id="test-input">
         Children
       </Input>,
     );
@@ -13,23 +13,83 @@ describe('<Input />', () => {
     expect(input).not.toBe(undefined);
   });
 
-  it('should render the label above the input by default', () => {
+  it('should render the label by default', () => {
     const { container } = render(
-      <Input className="test-input" id="test-input" label="Test input">
-        Children
+      <Input className="test-input" id="test-input">
+        Test input
       </Input>,
     );
-    const input = container.querySelector('.test-input');
-    expect(input).toHaveStyle('flex-direction: column;');
+    const input = container.querySelector('.test-input').textContent;
+    expect(input).toBe('Test input');
   });
 
-  it('should render the label beside the input when isInline', () => {
+  it('should render a text input by default', () => {
     const { container } = render(
-      <Input isInline className="test-input" id="test-input" label="Test input">
-        Children
+      <Input className="test-input" id="test-input">
+        Test input
       </Input>,
     );
-    const input = container.querySelector('.test-input');
-    expect(input).toHaveStyle('flex-direction: row;');
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('text');
+  });
+
+  it('should render an email input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="email">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('email');
+  });
+
+  it('should render a phone input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="phone">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('phone');
+  });
+
+  it('should render a number input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="number">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('number');
+  });
+
+  it('should render a currency input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="currency">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('currency');
+  });
+
+  it('should render a date input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="date">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('date');
+  });
+
+  it('should render a search input', () => {
+    const { container } = render(
+      <Input className="test-input" id="test-input" type="search">
+        Test input
+      </Input>,
+    );
+    const input = container.querySelector('.test-input-input').getAttribute('type');
+    expect(input).toBe('search');
   });
 });
--- packages/upstart/src/components/Input/Input.js
+++ packages/upstart/src/components/Input/Input.js
@@ -4,14 +4,13 @@ import PropTypes from 'prop-types';
 import { StyledLabel, TextInput } from './styled';
 
 export const Input = ({
-  id, isInline, type, label, placeholder, className, ...props
+  id, type, placeholder, className, children,
 }) => (
   <StyledLabel
-    isInline={isInline}
     htmlFor={id}
     className={className && `${className} ${className}-label`}
   >
-    {label}
+    {children}
     <TextInput
       type={type}
       name={id}
@@ -26,14 +25,16 @@ Input.propTypes = {
   /** A custom className you would like to pass to the Component */
   className: PropTypes.string,
 
+  /** The label text for the input */
+  children: PropTypes.oneOfType([
+    PropTypes.node,
+    PropTypes.arrayOf(
+      PropTypes.node,
+    ),
+  ]).isRequired,
+
   /** The unique identifier for the Input */
   id: PropTypes.string.isRequired,
-
-  /** If the label should be alongside the Input */
-  isInline: PropTypes.bool,
-
-  /** The text for the Input label */
-  label: PropTypes.string.isRequired,
 
   /** The placeholder text for the Input */
   placeholder: PropTypes.string,
@@ -52,7 +53,6 @@ Input.propTypes = {
 
 Input.defaultProps = {
   className: null,
-  isInline: false,
   placeholder: null,
   type: 'text',
 };
