774bfac24adf30f028c9055751b9eab01ce8f8ba
Dean Harris <dean@harris.tc>
2020-03-19 16:52:55 +1000

    feat: more token fixes

--- packages/tokens/builder/index.js
+++ packages/tokens/builder/index.js
@@ -1,35 +1,71 @@
 const fs = require("fs");
 
-const tokenReducer = (acc, cur) => {
-  acc[cur.name] = cur.value;
-  if (cur.aliases) {
-    cur.aliases.map(alias => acc[alias] = cur.value)
+const capitaliseWord = (word) => {
+  return word.split('').map((char, ind) => ind === 0 ? char.toUpperCase() : char).join('')
+}
+
+const compileTokensToObj = (raw) => raw.tokens.reduce((result, {name, value, aliases}) => {
+  result[name] = value;
+  if (aliases) {
+    aliases.forEach(alias => result[alias] = value)
   }
-  return acc;
+  return result
+}, {})
+
+const compileToJS = (raw) => {
+  const tokens = JSON.stringify(compileTokensToObj(raw));
+  return `export const ${raw.group} = ${tokens}`
+};
+
+const compileToJSON = (raw) => {
+  return JSON.stringify(compileTokensToObj(raw));
+};
+
+const compileToCSS = (raw) => {
+  const prefix = raw.group;
+  const tokens = compileTokensToObj(raw);
+  const compiled = Object.entries(tokens).map(([name, value]) => `  --${prefix}${capitaliseWord(name)}: ${value};\n`).join('')
+  return `:root {\n${compiled}}`
 }
 
-const initialTransform = (tokenGroup) => tokenGroup.tokens.reduce(tokenReducer, {})
+const compileToSCSS = (raw) => {
+  const prefix = `$${raw.group}`;
+  const tokens = compileTokensToObj(raw);
+  return Object.entries(tokens).map(([name, value]) => `${prefix}${capitaliseWord(name)}: ${value};\n`).join('')
+};
 
-const toJS = (prefix, inputTokens) => {
-  const jsReducer = (acc, [name, value]) => {
-    acc[`${prefix}${name}`] = value;
-    return acc;
-  }
-  return {
-    [prefix]: Object.entries(inputTokens).reduce(jsReducer, {})
-  }
+const compile = ({
+  inputFilePath = null,
+  outputFilePath = null,
+  targetFormats = [
+    'css',
+    'scss',
+    'javascript',
+    'json',
+  ],
+}) => {
+  console.log(inputFilePath, outputFilePath, targetFormats);
+
+  const rawData = fs.readFileSync(inputFilePath, 'utf-8');
+
+  targetFormats.map(format => {
+    switch (format) {
+      case 'css':
+        const output = compileToCSS(rawData);
+        fs.writeFileSync(`${outputFilePath}.css`, output, 'utf-8');
+      case 'scss':
+        const output = compileToSCSS(rawData);
+        fs.writeFileSync(`${outputFilePath}.scss`, output, 'utf-8');
+      case 'javascript':
+        const output = compileToJS(rawData);
+        fs.writeFileSync(`${outputFilePath}.js`, output, 'utf-8');
+      case 'json':
+        const output = compileToJSON(rawData);
+        fs.writeFileSync(`${outputFilePath}.json`, output, 'utf-8');
+      default:
+        break;
+    }
+  })
 }
 
-const toCSS = (prefix, inputTokens) => {
-  const transformed = Object.entries(inputTokens).map(([name, value]) => `  --${prefix}-${name}: ${value};\n`);
-  return `:root {\n${transformed.join('')}}`
-}
-
-const compile = () => {
-  const tokens = initialTransform(raw);
-  const prefixes = [raw.group, ...raw.aliases]
-  const js = prefixes.map(prefix => toJS(prefix, tokens))
-  const css = prefixes.map(prefix => toCSS(prefix, tokens))
-
-  fs.writeFileSync('css.css', css[0]);
-}
+module.exports = compile;
