47760d4ac0f196c8d892e62f600fe00ebd2e63f8
Dean Harris <dean@harris.tc>
2020-03-19 21:40:37 +1000

    feat: start using some new tokens in theme

--- packages/upstart/src/components/Theme/Theme.js
+++ packages/upstart/src/components/Theme/Theme.js
@@ -1,49 +1,25 @@
+import { space } from '@deanacus/upstart-tokens/dist/space';
+import { fonts } from '@deanacus/upstart-tokens/dist/font-family';
+import { fontSizes } from '@deanacus/upstart-tokens/dist/font-size';
+import { fontWeights } from '@deanacus/upstart-tokens/dist/font-weight';
+import { width as widths } from '@deanacus/upstart-tokens/dist/width';
+import { breakpoints } from '@deanacus/upstart-tokens/dist/breakpoints';
+import { borderWidth as borderWidths } from '@deanacus/upstart-tokens/dist/border-width';
+import { borderRadius as radii } from '@deanacus/upstart-tokens/dist/border-radius';
+import { shadow } from '@deanacus/upstart-tokens/dist/shadows';
+import { zIndex } from '@deanacus/upstart-tokens/dist/zindex';
+// import { lineHeight as lineHeights } from '@deanacus/upstart-tokens/dist/line-height';
 import { rootVal, preferredUnit } from '../../tokens/root';
-import { space } from '../../tokens/space';
-import { fonts, fontSizes, fontWeights } from '../../tokens/type';
 import { colors } from '../../tokens/colors';
-import { widths, breakpoints } from '../../tokens/widths';
+
 import {
-  borders, borderWidths, radii, borderStyles,
+  borders, borderStyles,
 } from '../../tokens/borders';
 
 const lineHeights = {
   body: 1.5,
   heading: 1.125,
 };
-
-const zIndices = [
-  -10,
-  0,
-  10,
-  20,
-  30,
-  40,
-  50,
-  60,
-  70,
-  80,
-  90,
-  100,
-];
-
-const shadows = [
-  'none',
-  '0px 1px 3px rgba(0, 0, 0, 0.1)',
-  '0px 2px 4px rgba(0, 0, 0, 0.15)',
-  '0px 2px 7px rgba(0, 0, 0, 0.15)',
-  '0px 2px 10px rgba(0, 0, 0, 0.2)',
-];
-
-[
-  shadows.none,
-  shadows.sm,
-  shadows.md,
-  shadows.lg,
-  shadows.xl,
-] = shadows;
-
-const elevations = shadows;
 
 const headingStyle = {
   fontFamily: fonts.heading,
@@ -79,9 +55,8 @@ export const Theme = {
   borderWidths,
   borderStyles,
   radii,
-  zIndices,
-  shadows,
-  elevations,
+  zIndex,
+  shadow,
   space,
   variants: {
     ...fontStyles,
